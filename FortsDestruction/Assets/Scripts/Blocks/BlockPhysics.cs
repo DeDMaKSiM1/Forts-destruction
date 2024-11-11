9abf50085ebd2e29b5ceaafd2268a3c0d2b116fc
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-11-11 06:20:32 +0300

    Попытался в ECS

--- FortsDestruction/Assets/Scripts/Blocks/BlockPhysics.cs
+++ FortsDestruction/Assets/Scripts/Blocks/BlockPhysics.cs
@@ -5,26 +5,18 @@ namespace Assets.Scripts.Physics
     public class BlockPhysics : MonoBehaviour
     {
         private Rigidbody2D rb;
+        [SerializeField] private int heroEnemyCoefficient = 1;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody2D>();
         }
-        public void KinematicOn()
-        {
-            rb.bodyType = RigidbodyType2D.Kinematic;
-        }
-        public void KinematicOff()
+
+        public void PhysicsOn()
         {
             rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.AddForce(new(heroEnemyCoefficient * 0.5f * Random.Range(2f, 6f), 0.7f * Random.Range(-4f, 6f)));
         }
-        public void AddForceEnemySide()
-        {
-            rb.AddForce(new(-0.5f * Random.Range(2f, 6f), 0.7f * Random.Range(-4f, 6f)));
-        }
-        public void AddForceHeroSide()
-        {
-            rb.AddForce(new(0.5f * Random.Range(2f, 6f), 0.7f * Random.Range(-4f, 6f)));
-        }
+
     }
 }
