4e0b243cd823c939f9cb7867bac88f9833dee636
DeDMaKSiM1 <maxlex2012@gmail.com>
2024-11-12 13:10:40 +0300

    удалены скрипты в ECS

---
+++ FortsDestructionECS/Assets/Scripts/BlockComponent.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+namespace Assets.Scripts
+{
+    public struct BlockComponent : IComponentData { }
+
+}
+
---
+++ FortsDestructionECS/Assets/Scripts/Health.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace Assets.Scripts
+{
+    public struct Health : IComponentData
+    {
+        public int Value;
+    }
+    public struct ProjectileTag : IComponentData { }
+    public struct BlockTag : IComponentData { }
+}
